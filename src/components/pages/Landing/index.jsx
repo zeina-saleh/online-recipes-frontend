99ce4dba26771ceac1ade1152f762252575ff6a5
zeinaCodes <eye_likemoon@hotmail.com>
2023-08-21 02:32:49 +0300

    <client> changed landing page

--- src/components/pages/Landing/index.jsx
+++ src/components/pages/Landing/index.jsx
@@ -1,48 +1,35 @@
-import React from 'react'
-import axios from 'axios';
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import RecipeCard from '../../RecipeCard';
+import { sendRequest } from '../../../config/request';
+import { Link } from 'react-router-dom'; 
+import "./style.css";
 
 const Landing = () => {
+  const [recipes, setRecipes] = useState([]);
 
-  const [selectedImage, setSelectedImage] = useState(null);
-
-  const handleImageChange = (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        setSelectedImage(e.target.result); // This is the entire data URL
-      };
-      reader.readAsDataURL(file); // Read the image file as data URL
+  const fetchRecipes = async () => {
+    try {
+      const response = await sendRequest({ route: "/getRecipe", body: "" });
+      setRecipes(response);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
     }
-  };
+  }; 
 
-  const handleUpload = async () => {
-    if (selectedImage) {
-      const imageData = selectedImage.split(",")[1]; // Extract base64 data
-      try {
-        const response = await axios.post("/uploadImage", {
-          image: imageData, // Send the base64 data to the server
-        });
-        console.log(response.data.message);
-      } catch (error) {
-        console.error("Image upload error:", error);
-      }
-    }
-  };
+  useEffect(() => {
+    fetchRecipes();
+  }, []); 
 
   return (
-    <div>
-      <h1>Image Upload</h1>
-      <input type="file" accept="image/*" onChange={handleImageChange} />
-      {selectedImage && (
-        <div>
-          <img src={selectedImage} alt="Selected" style={{ maxWidth: '100px' }} />
-          <button onClick={handleUpload}>Upload</button>
-        </div>
-      )}
+    <div className="recipe-list">
+      {recipes.map(recipe => (
+        <Link key={recipe.id} to={`/landing/${recipe.id}`}>
+        <RecipeCard recipe={recipe} />
+        </Link>
+      ))}
     </div>
   );
-}
+};
 
-export default Landing
+export default Landing;
